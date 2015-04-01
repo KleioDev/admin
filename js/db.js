e66f4eef57832d22cea0dd9277f851e90d3a46bd
Luis Medina <luis.medina14@upr.edu>
2015-04-01 17:04:49 -0400

    Added video extra content

--- js/db.js
+++ js/db.js
@@ -73,12 +73,15 @@ exports.objects = [{
             {
                 id:1,
                 title: "Theme",
-                youtube_url:"https://www.youtube.com/watch?v=1nCqRmx3Dnw"
+                youtube_url:"https://www.youtube.com/watch?v=1nCqRmx3Dnw",
+                embed_id: "1nCqRmx3Dnw"
+
             },
             {
                 id:2,
                 title:"The Dance of Death",
-                youtube_url:"https://www.youtube.com/watch?v=zS1cLOIxsQ8"
+                youtube_url:"https://www.youtube.com/watch?v=zS1cLOIxsQ8",
+                embed_id: "zS1cLOIxsQ8"
             }
         ],
         image_content: [
