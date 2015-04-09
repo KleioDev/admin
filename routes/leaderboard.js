a5a0577cdfc6d2486328c60e43769a2f1bc5e6b7
Luis Medina <luis.medina14@upr.edu>
2015-04-09 14:57:40 -0400

    Fixed leadeboard reoutes

--- routes/leaderboard.js
+++ routes/leaderboard.js
@@ -10,35 +10,43 @@ var fs = require("fs");
  * Render the Leaderboards page.
  * This user information is specific to the leaderboards.
  */
-function *leaderboard(){
-    //console.log(db.table.users);
-    yield this.render("leaderboard", {
-        title : "Leaderboard",
-        users : db.table.users});
-}
-exports.leaderboard = function(){
 
-}
+exports.leaderboard = function(){
+    function *leaderboard(){
+        //console.log(db.table.users);
+        yield this.render("leaderboard", {
+            title : "Leaderboard",
+            users : db.table.users});
+    }
+    return leaderboard;
+};
 
 /**
  * Parse user information to reset their score to 0
  */
-function *reset_score(){
-    var post = yield parse(this);
-    for(var i = 0; i < db.table.users.length; i++){
-        if(post.id == db.table.users[i].id) db.table.users[i].score = 0;
+
+exports.reset_score = function(){
+    function *reset_score(){
+        var post = yield parse(this);
+        for(var i = 0; i < db.table.users.length; i++){
+            if(post.id == db.table.users[i].id) db.table.users[i].score = 0;
+        }
+        this.redirect("/leaderboard");
+
+
     }
-    this.redirect("/leaderboard");
-
-
-}
+    return reset_score;
+};
 
 /**
  * Reset all users' score to 0.
  */
-function *reset_leaderboard(){
-    for(var i = 0; i < db.table.users.length; i++){
-        db.table.users[i].score = 0;
+exports.reset_leaderboard = function(){
+    function *reset_leaderboard(){
+        for(var i = 0; i < db.table.users.length; i++){
+            db.table.users[i].score = 0;
+        }
+        this.redirect("/leaderboard");
     }
-    this.redirect("/leaderboard");
-}
+    return reset_leaderboard;
+};
