031282b59700940aa51c5e7a6b73beb3c7976260
Luis Medina <luis.medina14@upr.edu>
2015-05-05 00:43:18 -0400

    Analytics should be working, need data

--- server.js
+++ server.js
@@ -106,7 +106,13 @@ app.use(handlebars({
              * Displays user analytics.
              */
             morrisbar: function(obj){
-                return "<script>$(function() {Morris.Bar({element: 'morris-bar-chart',data: [{ y: '2006', a: 100, b: 90 }, { y: '2007', a: 75, b: 65 }, { y: '2008', a: 50, b: 40 }, { y: '2009', a: 75, b: 65 }, { y: '2010', a: 50, b: 40 }, { y: '2011', a: 75, b: 65 }, { y: '2012', a: 100, b: 90 }], xkey: 'y', ykeys: ['a', 'b'], labels: ['Active Users', 'Interactive Users'], hideHover: 'auto', resize: true});});</script>";
+                var string = '[';
+                for(var i = 0; i < obj.length; i++){
+                    string += JSON.stringify(obj[i]);
+                    if(i != obj.length - 1) string += ',';
+                }
+                string += ']';
+                return "<script>$(function() {Morris.Bar({element: 'morris-bar-chart',data: " + string + ", xkey: 'period', ykeys: ['active', 'interactive'], labels: ['Active Users', 'Interactive Users'], hideHover: 'auto', resize: true});});</script>";
 
             },
             /**
